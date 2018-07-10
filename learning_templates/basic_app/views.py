b10cedd10c82c312880fe88a7e375574a5ce8c69
Gaurav <gauravsb1998@gmail.com>
2018-07-10 16:00:17 +0530

    'first'

---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'basic_app/index.html')
+
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def relative(request):
+        return render(request,'basic_app/relative_url_templates.html')
+
+
+# Create your views here.
